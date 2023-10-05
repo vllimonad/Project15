06eb86392ee5422a9885c22bfe056c8aba1ad440
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-05 14:30:24 +0200

    Initial Commit

---
+++ project15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project15
+//
+//  Created by Vlad Klunduk on 05/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
