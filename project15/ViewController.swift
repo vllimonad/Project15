1c81c3174fcfffe6f6594ff660af4cfc246ad02a
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-05 15:11:00 +0200

    animations

--- project15/ViewController.swift
+++ project15/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 230, y: 384)
+        view.addSubview(imageView)
     }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
 
-
+        UIView.animate(withDuration: 1, delay: 0, animations: {
+            switch self.currentAnimation {
+            case 0: self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1: self.imageView.transform = .identity
+            case 2: self.imageView.transform = CGAffineTransform(translationX: -100, y: -100)
+            case 3: self.imageView.transform = .identity
+            case 4: self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5: self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default: break
+            }
+            
+        }, completion: { _ in 
+            sender.isHidden = false
+        })
+        
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
